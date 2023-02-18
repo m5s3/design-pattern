9fdd3343f5fb9dc9d088bf48b27c1aecf2aadf5d
박명수 <maksalizm@bagmyeongsuui-MacBookPro.local>
2023-02-18 16:32:11 +0900

    main 코드 정리

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,5 @@
 package org.example;
 
-import org.example.singleton.A;
-import org.example.singleton.B;
-import org.example.singleton.SocketClient;
-
 public class Main {
     public static void main(String[] args) {
 
